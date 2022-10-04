56dea1878f533e6005299e5ac372fd0cd4dbf2eb
Mikhail <88738523+M1SK1NG@users.noreply.github.com>
2022-10-04 11:21:06 +0300

    Add files via upload

---
+++ Пр 3/Class2.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Практическая_3
+{
+    class Class2
+    {
+        public static void InitArray(out double[,] matr, int column, int row)
+        {
+            Random Rnd; Rnd = new Random();
+            matr = new Double[row, column];
+            for (int i = 0; i < matr.GetLength(0); i++)
+            {
+                for (int j = 0; j < matr.GetLength(1); j++)
+                {
+                    matr[i, j] = Math.Round(Rnd.Next(-20, 20) + Rnd.NextDouble(),2);
+                }
+            }
+        }
+        
+        public static void CreateArray(out double[,] matr, int column, int row)
+        {
+            matr = new double[row, column];
+        }
+        public static void Raschet(double[,] matr, out string num)
+        {
+            int i, j, kol = 0;
+            num = "";
+            for (j = 0; j < matr.GetLength(1); j++)
+            {
+                for (i = 0; i < matr.GetLength(0); i++)
+                {
+
+                   double a = matr[i, j];
+                    if (a < 0)
+                    {
+                        kol++;
+                    }
+                    
+                }
+                if (kol == i)
+                {
+                    num=num+Convert.ToString(j+1)+",";
+                    
+                }
+                kol = 0;
+            }
+        }
+    }
+}
---
+++ Пр 3/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Практическая_3
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        double[,] matr;
+        private void Raschet_Click(object sender, RoutedEventArgs e)
+        {
+            Class2.Raschet(matr, out string num);
+            
+            if(num=="")
+            {
+                MessageBox.Show("Необходимых столбцов нет", "Ошибка", MessageBoxButton.OK);
+            }
+            else Num.Text = num;
+        }
+
+        private void Create_Click(object sender, RoutedEventArgs e)
+        {
+            if (Int32.TryParse(Row.Text, out int row) == true && Int32.TryParse(Column.Text, out int count) == true)
+            {
+                Class2.CreateArray(out matr, count, row);
+                dataGrid.ItemsSource = Class1.ToDataTable(matr).DefaultView;
+            }
+            else MessageBox.Show("Введите корректные значения", "Ошибка", MessageBoxButton.OK);
+        }
+
+        private void Init_Click(object sender, RoutedEventArgs e)
+        {
+            if (Int32.TryParse(Row.Text, out int row) == true && Int32.TryParse(Column.Text, out int count) == true)
+            {
+                Class2.InitArray(out matr, count, row);
+                dataGrid.ItemsSource = Class1.ToDataTable(matr).DefaultView;
+            }
+            else MessageBox.Show("Введите корректные значения", "Ошибка", MessageBoxButton.OK);
+        }
+    }
+}
